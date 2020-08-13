b7c0782db991cf7446694870b51b5d042ec6c6dc
wnj00524 <68168066+wnj00524@users.noreply.github.com>
2020-08-13 21:47:38 +0100

    Updated break code,

--- search_and_load.py
+++ search_and_load.py
@@ -1,31 +1,38 @@
+import time
+
 def search(letters, key_letter):
     a = set()
     word_list = load_words()
     print(f"Total of {len(word_list)} words found.")
     count = 0
+    st = 0
     for word in word_list:
         count = count + 1
         if len(word) > 3:
-            print(f"Checking word {count} of {len(word_list)}. {get_pct(count,len(word_list))}%")
+            print(f"Checking word {count} of {len(word_list)}. {get_pct(count, len(word_list))}%")
+            st = time.perf_counter()
             dict_word_letters = list(word)
             step1 = True
             has_key_letter = False
             keep_checking = True
             for letter in dict_word_letters:
-                while keep_checking:
-                    if letter not in letters:
-                        step1 = False
-                        keep_checking = False
-                    if letter == key_letter:
-                        has_key_letter = True
+
+                if letter not in letters:
+                    step1 = False
+                    break
+
+                if letter == key_letter:
+                    has_key_letter = True
+
                 if step1 and has_key_letter:
                     a.add(word)
 
-    result_write = open("out.txt","w")
+    result_write = open("out.txt", "w")
     for word in a:
         result_write.write(word + "\n")
     result_write.close()
-
+    end = time.perf_counter()
+    print(f"Checked in {end - st:0.4f} seconds")
 
 
 def get_pct(score, max):
